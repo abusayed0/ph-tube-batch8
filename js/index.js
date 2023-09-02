91e1d65db91f07274806e814bf294b5a2cf5db6c
abusayed0 <abusayed48876@gmail.com>
2023-09-02 20:11:14 +0600

    added categories dynamically

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 console.log("js/index.js");
+
+const loadCategories = async () => {
+    const url = `https://openapi.programming-hero.com/api/videos/categories`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        const categories = data.data;
+        displayCategories(categories);
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+const displayCategories = (categories) =>{
+    const categoriesContainer = document.getElementById("categories-container");
+    categories.forEach(category => {
+        console.log(category);
+        const {category_id, category:categoryName} = category;
+        console.log(category_id, categoryName);
+        const newBtn = document.createElement("button");
+        newBtn.classList.add("px-5", "py-2", "rounded", "text-lg", "primary-bg");
+        newBtn.innerText = categoryName;
+        categoriesContainer.appendChild(newBtn);
+    })
+}
+loadCategories();
